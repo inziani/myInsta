d390b1143da63bbe7fc1492d924096e93b359ef2
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-17 21:50:50 +0300

    add static pages config and colors

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+django.urls import reverse
 
-# Create your tests here.
+class HomaPageTests(SimpleTestCase):
+
+  def test_home_page_status(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_view_url_name(self):
+    response = self.client.get(reverse('home'))
+    self.assertEqual(response.status_code, 200)
+
+
