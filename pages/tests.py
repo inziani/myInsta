802402b51ee535d6c9b07012a6605e6a54c69f2b
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-18 17:58:58 +0300

    add navbar

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, SimpleTestCase
 from django.contrib.auth import get_user_model
-django.urls import reverse
+from django.urls import reverse
 
 class HomaPageTests(SimpleTestCase):
 
@@ -11,5 +11,24 @@ class HomaPageTests(SimpleTestCase):
   def test_view_url_name(self):
     response = self.client.get(reverse('home'))
     self.assertEqual(response.status_code, 200)
+
+  def test_view_template(self):
+    response = self.client.get(reverse('home'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'home.html')
+
+class SignUpPageTests(TestCase):
+  username = 'newuser'
+  email = 'newuser@gmail.com'
+
+  def test_signup_page_status_code(self):
+    response = self.client.get('/users/signup/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_signup_form(self):
+    new_user = get_user_model().objects.create_user(self.username, self.email)
+    self.assertEqual(get_user_model().objects.all().count(), 1)
+    self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+    self.assertEqual(get_user_model().objects.all()[0].email, self.email)
 
 
