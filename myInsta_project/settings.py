7051ebc5c81a6029aa0f2391324343ce73d01e5c
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-21 10:58:31 +0300

    start model changes

--- myInsta_project/settings.py
+++ myInsta_project/settings.py
@@ -136,3 +136,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home' 
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net' 
+EMAIL_HOST_USER = 'apikey' 
+EMAIL_HOST_PASSWORD = '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' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
