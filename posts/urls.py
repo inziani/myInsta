318d45161b83855f80b3fa13866aa8b9ec31561b
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-24 09:09:42 +0300

    Create new db, create, new migrations, new superuser, add enctype on delete and create forms

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from .views import PostListView, PostUpdateView, PostDetailView, PostDeleteView,
 urlpatterns = [
   path('<int:pk>/edit/', PostUpdateView.as_view(), name = 'post_edit'),
   path('<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
-  path('<int:pk>', PostDeleteView.as_view(), name = 'post_delete'),
+  path('<int:pk>/delete/', PostDeleteView.as_view(), name = 'post_delete'),
   path('new/', PostCreateView.as_view(), name = 'post_new'),
   path('', PostListView.as_view(), name = 'post_list'),
 ]
